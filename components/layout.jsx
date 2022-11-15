0cf28637a6b823fb1188b270b9465c66f23c9dad
Elouann Lucas <elouannlucas56@gmail.com>
2022-11-15 14:26:42 +0100

    Begin copy to clipboard

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
-const name = 'Gamed';
-export const siteTitle = 'Next.js Sample Website';
+const name = "Gamed";
+export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
     return (
@@ -16,22 +16,18 @@ export default function Layout({ children, home }) {
                 <meta
                     property="og:image"
                     content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle,
+                        siteTitle
                     )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
-            <header className='mb-2 flex flex-wrap items-center justify-between sm:w-full'>
+            <header className="mb-6 flex flex-wrap items-center justify-between sm:w-full">
                 <div className="-skew-x-12 px-3 text-3xl bg-purple font-poppins text-white ">
-                    <Link href="/">
-                        {name}
-                    </Link>
+                    <Link href="/">{name}</Link>
                 </div>
                 <div className="px-3 text-2xl bg-purple font-poppins text-white ">
-                    <Link href={'posts/first-post'}>
-                        About
-                    </Link>
+                    <Link href={"posts/first-post"}>About</Link>
                 </div>
             </header>
             <main>{children}</main>
