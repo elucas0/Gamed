ed0157973db1f3dc93084e7db8e33e67d9f6dc57
Elouann Lucas <elouannlucas56@gmail.com>
2022-11-21 19:21:20 +0100

    Fixed today's gamed ^^

--- pages/archive/gamed8.jsx
+++ pages/archive/gamed8.jsx
@@ -1,42 +1,30 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import styles from '../styles/utils.module.css';
+import Layout, { siteTitle } from '../../components/layoutArchive';
+import styles from '../../styles/utils.module.css';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import GuessButton from '../components/guessButton';
-import SearchBar from '../components/searchBar';
-import ShareButton from '../components/shareButton';
-import ImageButtons from '../components/imageButtons';
+import GuessButton from '../../components/guessButtonArchive';
+import SearchBar from '../../components/searchBar';
+import ImageButtons from '../../components/imageButtons';
 
 export default function Home() {
-    const [gamedNb, setGamedNb] = useState(8);
+    const gamedNb = 8;
     const [gameName, setGame] = useState("The Witcher 3");
-    const [currentImage, setImage] = useState("/images/" + gamedNb + "/01.jpg");
     const [value, setValue] = useState('');
     const [gameState, setGameState] = useState("playing");
-    const [results, setResults] = useState("Gamed #" + gamedNb + "\n🎮 ⬛ ⬛ ⬛ ⬛ ⬛ ⬛\n\nhttps://gamed-seven.vercel.app/ ");
+    const [image, setImage] = useState("/images/" + gamedNb + "/01.jpg");
     const [buttons, addButton] = useState(
         [
             { id: 1, number: 1 },
         ]);
 
     useEffect(() => {
-        // If the last stored game number is different of the current one, delete the state and start a new game
-        if (localStorage.getItem('gamedNb') != gamedNb) {
-            localStorage.removeItem('played');
-            localStorage.removeItem('gamedNb');
-        }
-        // If the last stored game number is the same as the current one, load the state
-        if (localStorage.getItem('played')) {
-            setGameState(localStorage.getItem('gameState'));
-            setImage("/images/" + gamedNb + "/0" + localStorage.getItem("currentImage") + ".jpg");
-            // Else, start a new game
-        } else {
-            localStorage.setItem("gamedNb", gamedNb);
-            setGameState("playing");
-            localStorage.setItem("gameState", gameState);
-        }
-
+        // if (localStorage.getItem('played')) {
+        //     localStorage.setItem('gameState', gameState);
+        // } else {
+        //     localStorage.setItem('played', true);
+        //     setGameState("playing");
+        // }
     });
 
     const RenderAttempts = () => {
@@ -51,11 +39,11 @@ export default function Home() {
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <div className='mb-6'>
+            <div>
                 <div className={styles.image}>
-                    <Image
+                    <Image className='mb-6'
                         priority
-                        src={currentImage}
+                        src={image}
                         height={720}
                         width={1280}
                         alt=""
@@ -65,11 +53,11 @@ export default function Home() {
             </div>
             <div>
                 <div className="flex justify-center mb-6 font-bold space-x-3">
-                    <ImageButtons setImage={setImage} buttons={buttons} gamedNb={gamedNb} />
+                    <ImageButtons setImage={setImage} buttons={buttons} mainGameName={gameName} />
                 </div>
                 {gameState == 'lost' &&
                     <div className='flex justify-center mb-6'>
-                        <h2 className='text-xl'>Better luck tomorrow, it was {gameName}</h2>
+                        <h2 className='text-xl'>It was {gameName}</h2>
                     </div>
                 }
                 {gameState == 'won' &&
@@ -77,9 +65,8 @@ export default function Home() {
                         <h2 className='text-xl'>GG, you have found {gameName}</h2>
                     </div>
                 }
-                {gameState != "playing"
-                    ? <ShareButton gamedNb={gamedNb} results={results} buttons={buttons} addButton={addButton} />
-                    : <>
+                {gameState == 'playing' &&
+                    <>
                         <div className="mb-6">
                             <SearchBar setValue={setValue} value={value} />
                         </div>
@@ -87,7 +74,7 @@ export default function Home() {
                             <RenderAttempts />
                         </div>
                         <div className='flex justify-center items-center mb-6'>
-                            <GuessButton gamedNb={gamedNb} setGameState={setGameState} setResults={setResults} results={results} buttons={buttons} addButton={addButton} value={value} gameName={gameName} setImage={setImage} />
+                            <GuessButton gamedNb={gamedNb} setGameState={setGameState} buttons={buttons} addButton={addButton} value={value} gameName={gameName} setImage={setImage} />
                         </div>
                     </>
                 }
