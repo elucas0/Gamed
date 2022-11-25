d126817f0cc0fa3a9678d6e513d49c313066dc17
Elouann Lucas <elouannlucas56@gmail.com>
2022-11-25 15:14:25 +0100

    feature progression is now stored locally

--- components/imageButtons.jsx
+++ components/imageButtons.jsx
@@ -3,7 +3,7 @@ import ImageSelectButton from "./imageSelectButton";
 export default function ImageButtons({ setImage, buttons, gamedNb }) {
     buttons = buttons.map((button) => {
         return (
-            <div className="" key={button.id}>
+            <div className="" key={button.number}>
                 <ImageSelectButton buttonNb={button.number} setImage={setImage} gamedNb={gamedNb} />
             </div >
         );
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,6 @@ import Layout, { siteTitle } from '../components/layout';
 import styles from '../styles/utils.module.css';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import GuessButton from '../components/guessButton';
 import SearchBar from '../components/searchBar';
 import ShareButton from '../components/shareButton';
 import ImageButtons from '../components/imageButtons';
@@ -12,42 +11,82 @@ export default function Home() {
     const [gamedNb, setGamedNb] = useState(12);
     const [gameName, setGame] = useState("Mass Effect Andromeda");
     const [currentImage, setImage] = useState("/images/" + gamedNb + "/01.jpg");
+    const [currentGuess, setGuess] = useState(1);
     const [value, setValue] = useState('');
     const [gameState, setGameState] = useState("playing");
     const [buttons, addButton] = useState(
         [
-            { id: 1, number: 1 },
+            { number: 1 },
         ]);
 
-    useEffect(() => {
-        // If the last stored game number is different of the current one, delete the state and start a new game
-        if (localStorage.getItem('gamedNb') != gamedNb) {
-            localStorage.removeItem('played');
-            localStorage.removeItem('results')
-            localStorage.setItem('gamedNb', gamedNb);
-            setImage("/images/" + gamedNb + "/01.jpg");
-            localStorage.setItem('currentImage', "1");
-            localStorage.setItem("results", "Gamed #" + gamedNb + "\n🎮 ⬛ ⬛ ⬛ ⬛ ⬛ ⬛\n\nhttps://gamed-seven.vercel.app/");
+    const setArrayLength = () => {
+        for (let i = buttons.length; i < parseInt(localStorage.getItem("currentGuess")); i++) {
+            addButton((buttons) => [...buttons, { number: i + 1 }]);
         }
-        // If the last stored game number is the same as the current one, load the state
+    };
+
+    useEffect(() => {
+        if (localStorage.getItem('gamedNb') != gamedNb) {
+            ResetStorageAndState();
+        }
         if (localStorage.getItem('played')) {
-            setGameState(localStorage.getItem('gameState'));
-            setImage("/images/" + gamedNb + "/0" + localStorage.getItem("currentImage") + ".jpg");
-            // Else, start a new game
+            RestoreStorageAndState();
         } else {
-            localStorage.setItem("gamedNb", gamedNb);
-            //setImage("/images/" + gamedNb + "/0" + localStorage.getItem("currentImage") + ".jpg");
-            setGameState("playing");
-            localStorage.setItem("gameState", gameState);
+            ResumeStorageAndState();
         }
 
     });
 
+    const ResetStorageAndState = () => {
+        setImage(`/images/${gamedNb}/01.jpg`);
+        setGuess(1);
+        localStorage.removeItem('played');
+        localStorage.removeItem('results')
+        localStorage.setItem('gamedNb', gamedNb);
+        localStorage.setItem('currentImage', "1");
+        localStorage.setItem("currentGuess", 1);
+        localStorage.setItem("results", `Gamed #${gamedNb} \n🎮 ⬛ ⬛ ⬛ ⬛ ⬛ ⬛\n\nhttps://gamed-seven.vercel.app/`);
+    }
+
+    const RestoreStorageAndState = () => {
+        setGameState(localStorage.getItem('gameState'));
+        setImage(`/images/${gamedNb}/0${localStorage.getItem("currentImage")}.jpg`);
+    }
+
+    const ResumeStorageAndState = () => {
+        setGameState("playing");
+        setArrayLength();
+        setGuess(parseInt(localStorage.getItem("currentGuess")));
+        setImage(`/images/${gamedNb}/0${localStorage.getItem("currentImage")}.jpg`);
+        localStorage.setItem("gamedNb", gamedNb);
+        localStorage.setItem("gameState", gameState);
+    }
+
     const RenderAttempts = () => {
-        if (buttons.length === 6) {
+        if (currentGuess === 6) {
             return (<h2 className='text-xl'>1 attempt remaining</h2>);
         }
         return (<h2 className='text-xl'>{7 - buttons.length} attempts remaining</h2>);
+    }
+
+    const guess = (value) => {
+        let results = localStorage.getItem('results');
+        if (value === gameName.toLowerCase()) {
+            localStorage.setItem("results", results.replace("⬛", "🟨"));
+            setGameState("won");
+            localStorage.setItem('gameState', "won");
+        } else if (buttons.length <= 5) {
+            addButton([...buttons, { number: buttons.length + 1 }]);
+            setImage(`/images/${gamedNb}/0${buttons.length + 1}.jpg`);
+            localStorage.setItem("currentImage", buttons.length + 1);
+            localStorage.setItem("results", results.replace("⬛", "🟪"));
+            localStorage.setItem("currentGuess", currentGuess + 1);
+            setGuess(currentGuess + 1);
+        } else {
+            localStorage.setItem("results", results.replace("⬛", "🟪"));
+            setGameState("lost");
+            localStorage.setItem('gameState', "lost");
+        }
     }
 
     return (
@@ -91,7 +130,21 @@ export default function Home() {
                             <RenderAttempts />
                         </div>
                         <div className='flex justify-center items-center mb-6'>
-                            <GuessButton gamedNb={gamedNb} setGameState={setGameState} buttons={buttons} addButton={addButton} value={value} gameName={gameName} setImage={setImage} />
+                            <a className={styles.cta} onClick={() => guess(value.toLowerCase())}>
+                                {value !== ''
+                                    ? <span className={styles.span}>GUESS</span>
+                                    : <span className={styles.span}>SKIP</span>
+                                }
+                                <span className={styles.span}>
+                                    <svg width="40px" height="40px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                                        <g id="arrow" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                            <path className={styles.one} d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z" fill="#FFFFFF"></path>
+                                            <path className={styles.two} d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z" fill="#FFFFFF"></path>
+                                            <path className={styles.three} d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z" fill="#FFFFFF"></path>
+                                        </g>
+                                    </svg>
+                                </span>
+                            </a>
                         </div>
                     </>
                 }
