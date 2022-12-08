d4dc6f77fef096f266489a6e46c29e10d60f347a
Elouann Lucas <elouannlucas56@gmail.com>
2022-12-08 11:53:27 +0100

    matched css archive

--- components/layoutArchive.jsx
+++ components/layoutArchive.jsx
@@ -7,7 +7,7 @@ export const siteTitle = "Gamed Archive - Day";
 
 export default function Layout({ children, home }) {
     return (
-        <div className="m-auto-center p-3 max-w-2xl">
+        <div className="my-2 mx-auto p-4 max-w-2xl">
             <Head>
                 <link rel="icon" href="/gamedicon.ico" />
                 <meta
@@ -16,16 +16,16 @@ export default function Layout({ children, home }) {
                 />
                 <meta name="og:title" content={siteTitle} />
             </Head>
-            <header className="mb-6 flex items-center justify-between sm:w-full">
+            <header className="mb-4 flex items-center justify-between sm:w-full">
                 <div>
                     <div className="-skew-x-12">
-                        <Link className="px-3 text-3xl bg-purple" href="/">{name}</Link>
-                        <Link className="px-3 text-3xl bg-yellow text-black" href="/archive">Archive</Link>
+                        <Link className="px-3 text-2xl bg-purple" href="/">{name}</Link>
+                        <Link className="px-3 text-2xl bg-yellow text-black" href="/archive">Archive</Link>
                     </div>
                 </div>
                 <div className="flex space-x-3">
                     <Link href={"../stats"}>
-                        <div className="p-2.5 bg-purple ">
+                        <div className="p-1.5 bg-purple ">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
@@ -33,7 +33,7 @@ export default function Layout({ children, home }) {
                         </div>
                     </Link>
                     <Link href={"../about"}>
-                        <div className="p-2.5 bg-purple ">
+                        <div className="p-1.5 bg-purple ">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                             </svg>
--- pages/archive/[id].jsx
+++ pages/archive/[id].jsx
@@ -1,26 +1,26 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../../components/layoutArchive';
-import styles from '../../styles/utils.module.css';
-import Image from 'next/image';
-import { useState } from 'react';
-import GuessButton from 'components/guessButtonArchive';
-import SearchBar from 'components/searchBar';
-import { useRouter } from 'next/router'
-import { useEffect } from 'react';
+import Head from "next/head";
+import Layout, { siteTitle } from "../../components/layoutArchive";
+import styles from "../../styles/utils.module.css";
+import Image from "next/image";
+import { useState } from "react";
+import GuessButton from "components/guessButtonArchive";
+import SearchBar from "components/searchBar";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const gamesArchive = require("../../public/data/archive.json");
 
 export default function ArchiveEntry() {
     const router = useRouter();
     if (router.isFallback) {
-        return <h1>Loading...</h1>
+        return <h1>Loading...</h1>;
     }
     const gamedNb = router.query.id;
-    const [gameName, setGameName] = useState('');
-    const [value, setValue] = useState('');
-    const [gameState, setGameState] = useState('playing');
-    const [image, setImage] = useState('/mock_image.jpg');
-    const [buttons, addButton] = useState([{ number: 1 },]);
+    const [gameName, setGameName] = useState("");
+    const [value, setValue] = useState("");
+    const [gameState, setGameState] = useState("playing");
+    const [image, setImage] = useState("/mock_image.jpg");
+    const [buttons, addButton] = useState([{ number: 1 }]);
     const guessData = {
         gameName: gameName,
         gamedNb: gamedNb,
@@ -32,24 +32,30 @@ export default function ArchiveEntry() {
     };
 
     useEffect(() => {
-        router.isReady ? setImage(`/images/${gamedNb}/01.jpg`) + setGameName(gamesArchive[gamedNb - 1].game_name) : null;
+        router.isReady
+            ? setImage(`/images/${gamedNb}/01.jpg`) +
+            setGameName(gamesArchive[gamedNb - 1].game_name)
+            : null;
     }, [router.isReady]);
 
     const RenderAttempts = () => {
         if (buttons.length === 6) {
-            return (<h2 className='text-xl'>1 essai restant</h2>);
+            return <h2 className="text-lg sm:text-xl">1 essai restant</h2>;
         }
-        return (<h2 className='text-xl'>{7 - buttons.length} essais restants</h2>);
-    }
+        return (
+            <h2 className="text-lg sm:text-xl">{7 - buttons.length} essais restants</h2>
+        );
+    };
 
     return (
         <Layout home>
             <Head>
-                <title>{siteTitle + ' ' + gamedNb}</title>
+                <title>{siteTitle + " " + gamedNb}</title>
             </Head>
             <div>
                 <div className={styles.image}>
-                    <Image className='mb-6'
+                    <Image
+                        className="mb-6"
                         priority
                         src={image}
                         height={720}
@@ -60,41 +66,48 @@ export default function ArchiveEntry() {
                 </div>
             </div>
             <div>
-                <div className="flex justify-center mb-6 font-bold space-x-3">
+                <div className="flex justify-center mb-3 font-bold space-x-2">
                     {buttons.map((button) => {
                         return (
                             <div key={button.number}>
-                                <button onClick={() => { setImage(`/images/${gamedNb}/0${button.number}.jpg`) }} className="bg-yellow px-3 py-1 sm:text-2xl -skew-x-12 text-black focus:bg-purple focus:text-white">
+                                <button
+                                    onClick={() => {
+                                        setImage(
+                                            `/images/${gamedNb}/0${button.number}.jpg`
+                                        );
+                                    }}
+                                    className="bg-yellow px-3 py-1 sm:text-xl -skew-x-12 text-black focus:bg-purple focus:text-white"
+                                >
                                     {button.number}
                                 </button>
                             </div>
                         );
                     })}
-                </div >
+                </div>
             </div>
-            {gameState == 'lost' &&
-                <div className='flex justify-center mb-6'>
-                    <h2 className='text-xl'>C'était {gameName}</h2>
+            {gameState == "lost" && (
+                <div className="flex justify-center mb-3">
+                    <h2 className="text-center text-lg">C'était {gameName}</h2>
                 </div>
-            }
-            {gameState == 'won' &&
-                <div className='flex justify-center mb-6'>
-                    <h2 className='text-xl'>GG, tu as trouvé {gameName}</h2>
+            )}
+            {gameState == "won" && (
+                <div className="flex justify-center mb-3">
+                    <h2 className="text-center text-lg">GG, tu as trouvé {gameName}</h2>
                 </div>
-            }
-            {gameState == 'playing' &&
+            )}
+            {gameState == "playing" && (
                 <>
-                    <div className="mb-6">
+                    <div className="mb-3">
                         <SearchBar setValue={setValue} value={value} />
                     </div>
-                    <div className='flex justify-center mb-6'>
+                    <div className="flex justify-center mb-3">
                         <RenderAttempts />
                     </div>
-                    <div className='flex justify-center items-center mb-6'>
+                    <div className="flex justify-center items-center mb-3">
                         <GuessButton {...guessData} />
                     </div>
                 </>
-            }
-        </Layout >
+            )}
+        </Layout>
     );
-};
+}
