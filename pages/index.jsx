a754e09aeeac0c0b2711cecb9cfe6d1528f7a4d2
Elouann Lucas <elouannlucas56@gmail.com>
2022-12-10 00:19:05 +0100

    Changed gamed

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,8 +9,8 @@ import ImageButtons from '../components/imageButtons';
 import GuessButton from '../components/guessButton';
 
 export default function Home() {
-    const gamedNb = 17;
-    const gameName = "Kingdom Come Deliverance";
+    const gamedNb = 18;
+    const gameName = "Elden Ring";
     const [currentImage, setImage] = useState(`/images/${gamedNb}/01.jpg`);
     const [currentGuess, setGuess] = useState(1);
     const [value, setValue] = useState('');
