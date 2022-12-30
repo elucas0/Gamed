5d0e8445edf3f91958dbe4c0465f373803b37608
Elouann Lucas <elouannlucas56@gmail.com>
2022-12-30 01:46:57 +0100

    changed gamed

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,8 +9,8 @@ import ImageButtons from '../components/imageButtons';
 import GuessButton from '../components/guessButton';
 
 export default function Home() {
-    const gamedNb = 25;
-    const gameName = "Until Dawn";
+    const gamedNb = 26;
+    const gameName = "Detroit Become Human";
     const [currentImage, setImage] = useState(`/images/${gamedNb}/01.jpg`);
     const [currentGuess, setGuess] = useState(1);
     const [value, setValue] = useState('');
