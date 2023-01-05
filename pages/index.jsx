da1a55be4bf50f925c822f4c7b1546411ac6cdcb
Elouann Lucas <elouannlucas56@gmail.com>
2023-01-05 16:04:45 +0100

    gamed 28

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,8 +9,8 @@ import ImageButtons from '../components/imageButtons';
 import GuessButton from '../components/guessButton';
 
 export default function Home() {
-    const gamedNb = 27;
-    const gameName = "The Last Of Us Part II";
+    const gamedNb = 28;
+    const gameName = "Star Wars Battlefront";
     const [currentImage, setImage] = useState(`/images/${gamedNb}/01.jpg`);
     const [currentGuess, setGuess] = useState(1);
     const [value, setValue] = useState('');
