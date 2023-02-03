55e288646f7a30784ae4dc45ecb47acf89acf2d0
Elouann Lucas <elouannlucas56@gmail.com>
2023-02-03 22:49:28 +0100

    gamed 36

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ import ImageButtons from '../components/imageButtons';
 import GuessButton from '../components/guessButton';
 
 export default function Home() {
-    const gamedNb = 35;
+    const gamedNb = 36;
     const gameName = "Star Wars Jedi Fallen Order";
     const [currentImage, setImage] = useState(`/images/${gamedNb}/01.jpg`);
     const [currentGuess, setGuess] = useState(1);
